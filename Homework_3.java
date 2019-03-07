acdc43f474bde2b77e2c7188c68639a36f21051b
overkri <48020850+overkri@users.noreply.github.com>
2019-03-07 17:41:29 +0300

    Update Homework_3.java

--- Homework_3.java
+++ Homework_3.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -6,8 +7,48 @@ public class Homework_3 {
         String B = "";
         String A = GuessWord(B);
         System.out.println(A);
-    }
+        int C=Calculator();
+        System.out.println(C);
 
+    }
+    public static int Calculator() {
+        String input=scanner.nextLine();
+        int len = input.length();
+        ArrayList list1 = new ArrayList();
+        ArrayList list2 = new ArrayList();
+        for (int i = 0; i < len; i++) {
+            if ((input.charAt(i) != '+') && (input.charAt(i) != '-')&& (input.charAt(i) != '*')&& (input.charAt(i) != '/')) {
+                if ((i + 1 <= len - 1)) {
+                    if ((input.charAt(i + 1) != '+') && (input.charAt(i + 1) != '-')) {
+                        String temp = "";
+                        temp = temp + input.charAt(i) + input.charAt(i + 1);
+                        int tempToInt = Integer.parseInt(temp);
+                        list1.add(tempToInt);
+                    }
+                    list1.add(input.charAt(i) - '0');
+                }
+            } else if ((input.charAt(i) == '+') && (input.charAt(i) == '-')&& (input.charAt(i) == '*')&& (input.charAt(i) == '/')){
+                list2.add(input.charAt(i));
+            } else {
+                System.out.println("Incorrect symbols or different operators, please enter a numbers and only one type of operator !");
+                return(-1);
+            }
+        }
+        int result = 0;
+        result = result + (int) list1.get(0);
+        for (int t = 0; t < list2.size(); t++) {
+            char oper = (char) list2.get(t);
+            if (oper == '+') {
+                result = result + (int) list1.get(t);
+            } else if (oper == '-') {
+                result = result - (int) list1.get(t);
+            } else if (oper == '*') {
+                result = result * (int) list1.get(t);
+            }else if (oper == '/') {
+                result = result / (int) list1.get(t);
+            }
+        }return result;
+    }
     private static Scanner scanner = new Scanner(System.in);
 
     public static String GuessWord(String input) {
@@ -16,15 +57,14 @@ public class Homework_3 {
         int a = random.nextInt(25);
         System.out.println("Я загадал слово, сможете его угадать?");
         String str = arr[a];
-        System.out.println(str);
         int b = 0;
         input = scanner.nextLine();
+        String answer_1 = "***************";
+        StringBuilder answer = new StringBuilder(answer_1);
         while (!str.equals(input)) {
-            String answer_1 = "***************";
-            StringBuilder answer = new StringBuilder(answer_1);
             if (str.length() >= input.length()) {
                 b = input.length();
-            }else b = str.length();
+            } else b = str.length();
             for (int i = 0; i < b; ++i) {
                 if (input.charAt(i) == str.charAt(i)) {
                     answer.setCharAt(i, input.charAt(i));
